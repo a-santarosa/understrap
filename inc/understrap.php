ab7e162cc92caa18f9ba73546f6137072a0f4db0
Anthony Santarosa <anthony@guhru.com>
2015-12-07 17:01:20 -0500

    Initial Commit

---
+++ inc/understrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Enqueue scripts and styles.
+ */
+function _strap_scripts() {
+
+  /* Add Bootstrap CSS */
+  wp_enqueue_style('_s-bootstrap', get_template_directory_uri() . '/bootstrap/dist/css/bootstrap.min.css' );
+  /* Add Custom CSS */
+  wp_enqueue_style( '_s-style', get_stylesheet_uri() );
+  /* Add Bootstrap JS */
+  wp_enqueue_script( '_s-bootstrap-js', get_template_directory_uri() . '/bootstrap/dist/js/bootstrap.min.js', array('jquery'), '', true );
+  /* Add Custom JS */
+  wp_enqueue_script( '_s-strap-js', get_template_directory_uri() . '/js/strap.js', array('jquery'), '', true );
+
+}
+add_action( 'wp_enqueue_scripts', '_s_scripts' );
+
+// Register Custom Navigation Walker
+require get_template_directory() . '/wp-bootstrap-navwalker/wp_bootstrap_navwalker.php';
---
+++ js/understrap.js
@@ -0,0 +1,8 @@
+/** Custom JS to adjust styles */
+
+( function( $ ) {
+
+  $('.content-area').addClass('col-xs-12 col-md-8');
+  $('.widget-area').addClass('col-xs-12 col-md-4');
+
+})( jQuery );
