705a9eb4b365d799ce478ffb7a78a2141e4e5cad
Anthony Santarosa <anthony@guhru.com>
2015-12-07 19:49:51 -0500

    Add wp-navwalker code

--- header.php
+++ header.php
@@ -41,10 +41,42 @@
 			endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+<nav class="site-navigation">
+				<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+					<div class="container">
+						<div class="row">
+							<div class="site-navigation-inner col-sm-12">
+								<div class="navbar navbar-default">
+									<div class="navbar-header">
+									<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
+									<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+										<span class="sr-only"><?php _e('Toggle navigation','_tk') ?> </span>
+									  <span class="icon-bar"></span>
+									  <span class="icon-bar"></span>
+									  <span class="icon-bar"></span>
+									</button>
+
+									<!-- Your site title as branding in the menu -->
+									<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+								  </div>
+
+								<!-- The WordPress Menu goes here -->
+							<?php wp_nav_menu(
+								array(
+									'theme_location' => 'primary',
+									'container_class' => 'collapse navbar-collapse navbar-responsive-collapse',
+									'menu_class' => 'nav navbar-nav',
+									'fallback_cb' => '',
+									'menu_id' => 'main-menu',
+									'walker' => new wp_bootstrap_navwalker()
+								)
+							); ?>
+
+								</div><!-- .navbar -->
+							</div>
+						</div>
+					</div><!-- .container -->
+			</nav><!-- .site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
