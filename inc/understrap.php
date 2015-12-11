98bba6221e7171fa565fac3683f44bb956e05c1d
Anthony Santarosa <anthony@guhru.com>
2015-12-10 20:21:42 -0500

    Move bootsrap and update understrap.php

--- inc/understrap.php
+++ inc/understrap.php
@@ -9,12 +9,12 @@ function _strap_scripts() {
   /* Add Custom CSS */
   wp_enqueue_style( '_s-style', get_stylesheet_uri() );
   /* Add Bootstrap JS */
-  wp_enqueue_script( '_s-bootstrap-js', get_template_directory_uri() . '/bootstrap/dist/js/bootstrap.min.js', array('jquery'), '', true );
+  wp_enqueue_script( '_s-bootstrap-js', get_template_directory_uri() . '/bootstrap/dist/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
   /* Add Custom JS */
-  wp_enqueue_script( '_s-strap-js', get_template_directory_uri() . '/js/strap.js', array('jquery'), '', true );
+  wp_enqueue_script( '_s-strap-js', get_template_directory_uri() . '/js/strap.js', array('jquery'), '0.1', true );
 
 }
-add_action( 'wp_enqueue_scripts', '_s_scripts' );
+add_action( 'wp_enqueue_scripts', '_strap_scripts' );
 
 // Register Custom Navigation Walker
 require get_template_directory() . '/wp-bootstrap-navwalker/wp_bootstrap_navwalker.php';
